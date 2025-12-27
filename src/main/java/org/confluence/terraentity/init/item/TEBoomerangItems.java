fe94210ccdbf7ec7a11998a4b6d1178ae1b10841
voila <voila1106@gmail.com>
2025-12-27 20:28:09 +0800

    改个烈焰回旋镖粒子

--- src/main/java/org/confluence/terraentity/init/item/TEBoomerangItems.java
+++ src/main/java/org/confluence/terraentity/init/item/TEBoomerangItems.java
@@ -6,12 +6,15 @@ import net.minecraft.world.item.Item;
 import net.neoforged.neoforge.registries.DeferredItem;
 import net.neoforged.neoforge.registries.DeferredRegister;
 import org.apache.commons.lang3.function.TriFunction;
+import org.confluence.lib.common.particle.CrossDustParticleOptions;
 import org.confluence.terraentity.TerraEntity;
 import org.confluence.terraentity.data.component.SingleBooleanComponent;
 import org.confluence.terraentity.entity.util.trail.BoomerangTrail;
 import org.confluence.terraentity.init.TEDataComponentTypes;
 import org.confluence.terraentity.init.TEEffectStrategies;
 import org.confluence.terraentity.item.Boomerang;
+import org.joml.Vector3f;
+import org.joml.Vector4f;
 
 
 public class TEBoomerangItems {
@@ -29,22 +32,23 @@ public class TEBoomerangItems {
             new Boomerang.BoomerangModifier().setDurability(100)
     );
     public static final DeferredItem<Boomerang> ENCHANTED_BOOMERANG = register("enchanted_boomerang",6.6F,
-            NORMAL_BOOMERANG_MODIFIER.apply(15,1.55f,1.55f).setDurability(300)
+            NORMAL_BOOMERANG_MODIFIER.apply(15,1.55f,1.55f).setDurability(300).setLuminance(5)
     );
     public static final DeferredItem<Boomerang> SHROOMERANG = register("shroomerang",8.6F,
-            NORMAL_BOOMERANG_MODIFIER.apply(15,1.55f,1.55f).setDurability(500)
+            NORMAL_BOOMERANG_MODIFIER.apply(15,1.55f,1.55f).setDurability(500).setLuminance(5)
     );
     public static final DeferredItem<Boomerang> ICE_BOOMERANG = register("ice_boomerang",8.3F,
             NORMAL_BOOMERANG_MODIFIER.apply(16,1.6f,1.6f)
                     .setOnHitEffect(TEEffectStrategies.Components.FROST_BURN_BOOMERANG_EFFECT.get())
                     .setDurability(500)
                     .setTrail(()->new BoomerangTrail(20, 0x2553be, 0x255385))
-                    .setParticle(()->ParticleTypes.SNOWFLAKE)
+                    .setLuminance(5)
+                    .setParticle(e->ParticleTypes.SNOWFLAKE)
     );   //50% 的几率造成 3 秒的霜冻。
 
     public static final DeferredItem<Boomerang> TRIMARANG = register("trimarang",8.3F,
             MULTI_BOOMERANG_MODIFIER.apply(10,3,
-                    NORMAL_BOOMERANG_MODIFIER.apply(17,1.85f,1.85f)).setDurability(1000)
+                    NORMAL_BOOMERANG_MODIFIER.apply(17,1.85f,1.85f)).setDurability(1000).setLuminance(6)
     );   // 使用三次
 
     public static final DeferredItem<Boomerang> COMBAT_WRENCH = register("combat_wrench",9.0F,
@@ -55,8 +59,28 @@ public class TEBoomerangItems {
             NORMAL_BOOMERANG_MODIFIER.apply(18,1.85f,1.85f)
                     .setOnHitEffect(TEEffectStrategies.Components.HELL_FIRE_EFFECT.get())
                     .setDurability(1500)
+                    .setLuminance(7)
                     .setTrail(()->new BoomerangTrail(200, 0xFB6464, 0xbc2525))
-                    .setParticle(()->ParticleTypes.LAVA)
+                    .setParticle(e -> {
+                        int r = e.getRandom().nextInt(3);
+                        int centerColor;
+                        int edgeColor;
+                        if (r == 0) {
+                            centerColor = 0xffFFFF53;
+                            edgeColor = 0xefFF7A53;
+                        } else if (r == 1) {
+                            centerColor = 0xffFFFF53;
+                            edgeColor = 0xffFFB552;
+                        } else {
+                            centerColor = 0xffFFB552;
+                            edgeColor = 0xefFF7A53;
+                        }
+                        Vector3f velocity = e.getDeltaMovement().normalize().scale(0.04).toVector3f();
+                        Vector4f curve = new Vector4f(0, 1, 1, 1);
+                        return new CrossDustParticleOptions(false, centerColor, edgeColor, velocity,
+                            curve, e.getRandom().nextFloat() * 0.3f + 1.2f, 11, velocity.x > 0 ? 10 : -10,
+                            curve, true, true, true, false);
+                    }, 2)
     );   //狱炎效果。
 
 
@@ -66,6 +90,7 @@ public class TEBoomerangItems {
                     .setFlySpeedFactor(2) // 设置向前飞行速度
                     .setBackSpeedFactor(2f) // 设置后退速度
                     .setCanPenetrate()
+                    .setLuminance(6)
                     .setMaxCount(10)
     );
 
