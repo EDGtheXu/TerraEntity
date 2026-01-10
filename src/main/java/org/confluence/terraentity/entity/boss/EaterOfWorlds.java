40a4b6e8dc572cb5bf502967302c0d1c37b9eb50
王小尽 <1809315974@qq.com>
2026-01-10 09:51:09 +0800

    将TaskScheduler从TerraEntity模块迁移到Lib模块

--- src/main/java/org/confluence/terraentity/entity/boss/EaterOfWorlds.java
+++ src/main/java/org/confluence/terraentity/entity/boss/EaterOfWorlds.java
@@ -17,6 +17,7 @@ import net.minecraft.world.entity.ai.targeting.TargetingConditions;
 import net.minecraft.world.entity.monster.Monster;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.phys.Vec3;
+import org.confluence.lib.util.TaskScheduler;
 import org.confluence.terraentity.api.entity.Boss;
 import org.confluence.terraentity.api.entity.IHeightControlMob;
 import org.confluence.terraentity.config.ServerConfig;
@@ -28,7 +29,6 @@ import org.confluence.terraentity.registries.mappeddata.MappedDataTypes;
 import org.confluence.terraentity.utils.CameraShakeData;
 import org.confluence.terraentity.utils.CameraShakeManager;
 import org.confluence.terraentity.utils.TEUtils;
-import org.confluence.terraentity.utils.TaskScheduler;
 
 import java.util.List;
 import java.util.Objects;
